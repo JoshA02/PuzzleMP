fe292c0cec18801ec303489638cf69f2bb97f7fe
Josh <joshuav2012@live.co.uk>
2022-03-18 15:35:40 +0000

    Level 1: Button panel and laser basic setup

--- Source/PuzzleMP/L1_ButtonPanel.cpp
+++ Source/PuzzleMP/L1_ButtonPanel.cpp
@@ -3,19 +3,41 @@
 
 #include "L1_ButtonPanel.h"
 
+#include "MyPlayerController.h"
+#include "Engine/StaticMeshActor.h"
+#include "Kismet/GameplayStatics.h"
+
 // Sets default values
 AL1_ButtonPanel::AL1_ButtonPanel()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ButtonPanel = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SM_ButtonPanel"));
+	ButtonPanel->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
 void AL1_ButtonPanel::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	if(Laser == nullptr)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Laser actor not provided"))
+		return;
+	}
+	UE_LOG(LogTemp, Log, TEXT("Specified laser found"))
+
+	AMyPlayerController* HostPlayerController;
+	//Get host
+	if(HasAuthority()) HostPlayerController = Cast<AMyPlayerController>(UGameplayStatics::GetPlayerController(GetWorld(), 0));
+	else
+	{
+		UE_LOG(LogTemp, Log, TEXT("Cannot find host player. Puzzle cannot continue"));
+		return;
+	}
+
+	//
 }
 
 // Called every frame
--- Source/PuzzleMP/L1_ButtonPanel.h
+++ Source/PuzzleMP/L1_ButtonPanel.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "L1_Laser.h"
+#include "Engine/StaticMeshActor.h"
 #include "GameFramework/Actor.h"
 #include "L1_ButtonPanel.generated.h"
 
@@ -15,9 +17,16 @@ public:
 	// Sets default values for this actor's properties
 	AL1_ButtonPanel();
 
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly)
+//	TSubclassOf<AL1_Laser> Laser;
+	AL1_Laser* Laser;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* ButtonPanel;
 
 public:	
 	// Called every frame
